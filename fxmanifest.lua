ed050ecb31d6a0f6c4b4d9e7313f1e01892ed28a
Tuna Terps <62733984+Tuna-Terps@users.noreply.github.com>
2023-04-09 18:54:35 -0400

    fxmanifest.lua conversion

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,11 @@
+fx_version 'bodacious'
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+games { 'gta5' }
+
+author 'EthanPeacock'
+description 'progressBars'
+version '1.0.0'
+
 ui_page 'h.html'
 
 client_scripts {
